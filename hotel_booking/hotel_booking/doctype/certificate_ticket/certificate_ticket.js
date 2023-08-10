d2b1e7868de315c4c9e8b1e30ab9b5c4aca4f40e
prabhuvermaa <you@example.com>
2023-08-10 07:05:04 +0000

    test commit

---
+++ hotel_booking/hotel_booking/doctype/certificate_ticket/certificate_ticket.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Prabhu Verma and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Certificate Ticket', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ hotel_booking/hotel_booking/doctype/certificate_ticket/test_certificate_ticket.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Prabhu Verma and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCertificateTicket(FrappeTestCase):
+	pass
